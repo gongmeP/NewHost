1ff9a0e2ff347fadad8f817505cdb5c89c39586a
균 <test@testui-MacBookAir.local>
2025-01-02 04:31:11 +0900

    Feat : new project setting

---
+++ Desktop/newhostmonitor/host_back/src/main/java/com/example/hostmonitor/HostmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.hostmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostmonitorApplication.class, args);
+	}
+
+}
